69473c23c5450d25d7d8e18d4fa52a1f1254b7ba
xz <812964793@qq.com>
2020-07-14 22:31:13 +0800

    pytest测试实战（二）作业

--- testing/conftest.py
+++ testing/conftest.py
@@ -9,8 +9,51 @@ def calc_remind():
     yield
     print("\n计算结束\n")
 
+
 @pytest.fixture()
 def login(request):
     user = request.param
     print(f"登陆用户为{user}")
     return user
+
+#自定义动态生成测试用例fixture
+def pytest_generate_tests(metafunc: "Metafunc"):
+    if "add" in metafunc.fixturenames:
+        metafunc.parametrize("add", metafunc.module.adddatas
+                             , ids=metafunc.module.addids
+                             , scope="function")
+    if "dec" in metafunc.fixturenames:
+        metafunc.parametrize("dec", metafunc.module.decdatas
+                             , ids=metafunc.module.decids
+                             , scope="function")
+    if "div" in metafunc.fixturenames:
+        metafunc.parametrize("div", metafunc.module.divdatas
+                             , ids=metafunc.module.divids
+                             , scope="function")
+    if "mul" in metafunc.fixturenames:
+        metafunc.parametrize("mul", metafunc.module.muldatas
+                             , ids=metafunc.module.mulids
+                             , scope="function")
+
+def pytest_addoption(parser):
+    mygroup = parser.getgroup("Lie-tester")     #group 将下面所有的 option都展示在这个group下。
+    mygroup.addoption("--env",    #注册一个命令行选项
+                      default='st',
+                      dest='env',
+                      help='set your run env'
+                      )
+
+@pytest.fixture(scope='session')
+def mycmdoption(request):
+    myenv = request.config.getoption("--env", default='st')
+    if myenv == "test":
+        datapath = 'env/test/data.yml'
+    if myenv == "dev":
+        datapath = 'env/dev/data.yml'
+    if myenv == "st":
+        datapath = 'env/st/data.yml'
+
+    with open(datapath) as f:
+        datas = yaml.safe_load(f)
+
+    return myenv, datas
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -8,8 +8,19 @@ import yaml
 print(sys.path.append('..'))
 from pythoncode.calc import Calculator
 
+with open("./data/data.yml") as f:
+    datas = yaml.safe_load(f)
+    addids = datas["add"].keys()
+    adddatas = datas["add"].values()
+    decids = datas["dec"].keys()
+    decdatas = datas["dec"].values()
+    divids = datas["div"].keys()
+    divdatas = datas["div"].values()
+    mulids = datas["mul"].keys()
+    muldatas = datas["mul"].values()
 
-@allure.feature("计算器")
+
+# @allure.feature("计算器")
 class TestCalc:
 
     def setup_class(self):
@@ -18,70 +29,90 @@ class TestCalc:
     # def teardown_class(self):
     #     self.cal = Calculator()
 
-    @pytest.mark.parametrize("a, b", yaml.safe_load(open("./data/add.yaml"))
-        , ids=["int", "minus", "float", "bigint", "str", "str and int"])
-    @pytest.mark.add
-    @allure.story("加法运算")
-    def test_add(self, a, b):
-        if type(a) == str or type(b) == str:
+    # @pytest.mark.parametrize("a, b", adddatas
+    #     , ids=addids)
+    # @pytest.mark.add
+    # @allure.story("加法运算")
+    @pytest.mark.first
+    @pytest.mark.dependency(name='add')
+    def test_add(self, add):
+        if type(add[0]) == str or type(add[1]) == str:
             try:
-                self.cal.add(a, b)
+                self.cal.add(add[0], add[1])
             except Exception:
                 assert True
             else:
                 assert False
         else:
-            result = a + b
-            assert self.cal.add(a, b) == result
+            result = add[0] + add[1]
+            assert self.cal.add(add[0], add[1]) == result
 
-    @pytest.mark.parametrize("a, b", yaml.safe_load(open("./data/dec.yaml"))
-        , ids=["int", "minus", "float", "bigint", "str", "str and int"])
-    @pytest.mark.dec
-    @allure.story("减法运算")
-    def test_dec(self, a, b):
-        if type(a) == str or type(b) == str:
+    # @pytest.mark.parametrize("a, b", decdatas
+    #     , ids=decids)
+    # @pytest.mark.dec
+    # @allure.story("减法运算")
+    @pytest.mark.second
+    @pytest.mark.dependency(depends=["add"])
+    def test_dec(self, dec):
+        if type(dec[0]) == str or type(dec[1]) == str:
             try:
-                self.cal.dec(a, b)
+                self.cal.dec(dec[0], dec[1])
             except Exception:
                 assert True
             else:
                 assert False
         else:
-            result = a - b
-            assert self.cal.dec(a, b) == result
+            result = dec[0] - dec[1]
+            assert self.cal.dec(dec[0], dec[1]) == result
 
-    @pytest.mark.parametrize("a, b", yaml.safe_load(open("./data/mul.yaml"))
-        , ids=["int", "minus", "float", "bigint", "str", "str and int"])
-    @pytest.mark.mul
-    @allure.story("乘法运算")
-    def test_mul(self, a, b):
-        if type(a) == str or type(b) == str:
+    # @pytest.mark.parametrize("a, b", muldatas
+    #     , ids=mulids)
+    # @pytest.mark.mul
+    # @allure.story("乘法运算")
+    @pytest.mark.third
+    @pytest.mark.dependency(name='mul')
+    def test_mul(self, mul):
+        if type(mul[0]) == str or type(mul[1]) == str:
             try:
-                self.cal.mul(a, b)
+                self.cal.mul(mul[0], mul[1])
             except Exception:
                 assert True
             else:
                 assert False
         else:
-            result = a * b
-            assert self.cal.mul(a, b) == result
+            result = mul[0] * mul[1]
+            assert self.cal.mul(mul[0], mul[1]) == result
 
-    @pytest.mark.parametrize("a, b", yaml.safe_load(open("./data/div.yaml"))
-        , ids=["int", "minus", "float", "bigint", "str", "str and int","zero"])
-    @pytest.mark.div
-    @allure.story("除法运算")
-    def test_div(self, a, b):
-        if type(a) == str or type(b) == str or b == 0:
+    # @pytest.mark.parametrize("a, b", divdatas
+    #     , ids=divids)
+    # @pytest.mark.div
+    # @allure.story("除法运算")
+    @pytest.mark.fourth
+    @pytest.mark.dependency(depends=["mul"])
+    def test_div(self, div):
+        if type(div[0]) == str or type(div[1]) == str or div[1] == 0:
             try:
-                self.cal.div(a, b)
+                self.cal.div(div[0], div[1])
             except Exception:
                 assert True
             else:
                 assert False
         else:
-            result = a / b
-            assert self.cal.div(a, b) == result
+            result = div[0] / div[1]
+            assert self.cal.div(div[0], div[1]) == result
 
+    @pytest.mark.first
+    def check_test(self):
+        print("check_*开头的用例也会执行")
 
-if __name__ == '__main__':
-    pytest.main()
+    def test_env(self,mycmdoption):
+        print("环境验证")
+        myenv, datas = mycmdoption
+        print(f"环境：{myenv} , 数据：{datas}")
+        ip = datas['env']['ip']
+        port = datas['env']['port']
+        url = 'http://'+str(ip)+':'+str(port)
+        print(url)
+
+    if __name__ == '__main__':
+        pytest.main()
